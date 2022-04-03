16c697940259c5e5572a7843ddad1f99cfe22baf
Aashvi Kothari <aashvikothari78@gmail.com>
2022-04-03 17:21:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+// import { Row } from 'react-bootstrap';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Routes>
+            <Route path='/' element={<Login />}/>
+            <Route path='/signup' element={<Signup />}/>
+          </Routes>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
